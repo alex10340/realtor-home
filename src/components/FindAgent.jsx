10482136296703c8e9fe094c5c8541a74f45f893
boogie531 <alexandre.pir.silva@gmail.com>
2024-07-03 21:40:37 +0100

    find agent

--- src/components/FindAgent.jsx
+++ src/components/FindAgent.jsx
@@ -10,16 +10,54 @@ const FindAgent = () => {
           </figure>
         </div>
         <div className="px-10 py-10 sm:w-[60vw]">
-          <h2 className="text-4xl font-bold">
+          <h2 className="mb-2 text-4xl font-bold prose">
             Find your agent{" "}
             <span className="underline text-neutral">today</span>
           </h2>
-          <div className="py-5 opacity-85">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-            eveniet sed in cupiditate sit tempore vel! Accusamus, aspernatur
-            nihil? Eos laudantium quisquam, id esse voluptatum placeat unde
-            laborum eius voluptatibus amet tenetur ad saepe? Est voluptatem
-            veritatis aspernatur corrupti placeat?
+          <div className="py-5 opacity-85 prose">
+            <p>
+              Looking to buy or sell a property? Our experienced team of real
+              estate agents is here to help you every step of the way. Whether
+              you're searching for your dream home or looking to list your
+              property, our dedicated agents are committed to providing
+              exceptional service tailored to your needs.
+            </p>
+            <h3>Why Choose Us?</h3>
+            <ul>
+              <li>
+                <p>
+                  <strong>Expertise:</strong> Our agents have in-depth knowledge
+                  of the local market and trends.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <strong>Personalized Service: </strong> We take the time to
+                  understand your unique requirements.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <strong>Proven Track Record: </strong> We have successfully
+                  helped numerous clients achieve their real estate goals.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <strong>Local Insights: </strong> Gain insights into
+                  neighborhoods, schools, and amenities.
+                </p>
+              </li>
+            </ul>
+            <h3>Get Started Today</h3>
+            <p>
+              Contact us to connect with one of our friendly agents who will
+              guide you through the process with professionalism and care. Your
+              perfect property or buyer is just a call away!
+            </p>
+            <button className="text-white btn btn-wide btn-primary">
+              Contact us
+            </button>
           </div>
         </div>
       </div>
