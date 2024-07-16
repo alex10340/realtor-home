3f52a1cab7e12248b271b491ac0bafff405ffa38
boogie531 <alexandre.pir.silva@gmail.com>
2024-07-16 12:01:56 +0100

    agency section

--- src/components/AgencySection.jsx
+++ src/components/AgencySection.jsx
@@ -1,12 +1,12 @@
 const AgencySection = ({ logo, name, desc }) => {
   return (
-    <div className="flex items-center px-6">
+    <div className="flex items-center lg:px-6">
       <div>
-        <div className="px-8 text-xl cursor-default lg:text-2xl hover:bg-white btn btn-ghost no-animation">
+        <div className="flex gap-2 items-center p-0 text-lg font-bold cursor-default md:pl-8 lg:text-xl hover:bg-white no-animation">
           <img src={logo} alt="Agency Logo" className="w-12" />
           <p>AcmeHome {name}</p>
         </div>
-        <div className="px-10 py-5 opacity-85 prose">
+        <div className="px-1 pt-5 md:px-10 opacity-85 prose">
           <p className="pb-5">{desc}</p>
 
           <button className="text-white shadow-lg btn btn-wide btn-primary">
