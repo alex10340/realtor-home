ed0d30538ac1a7d61b391afa587117130281ba23
alex103040 <alexandre.pir.silva@gmail.com>
2024-07-22 12:25:02 +0100

    padding fix

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,7 +3,7 @@ import { locations } from "../data/allData";
 
 const Grid = () => {
   return (
-    <div className="py-6 pb-10 px-[12px] mx-auto max-w-[1350px]">
+    <div className="py-6 pb-12 px-[12px] mx-auto max-w-[1350px]">
       <h3 className="p-2 pt-0 pb-4 text-2xl font-normal">Most popular areas</h3>
       <div className="grid grid-cols sm:grid-cols-2 min-[1200px]:grid-cols-3">
         {locations.map((location) => (
